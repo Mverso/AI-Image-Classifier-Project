0dcbafd4fef0f799e1764518e0297e10c6524ca7
Mark-Verso <markaverso@gmail.com>
2020-05-20 01:13:08 -0700

    Add arguement parser to predict.py

--- predict.py
+++ predict.py
@@ -19,3 +19,39 @@ filepath = 'cat_to_name.json'
 arch=''
 image_path = "flowers/test/10/image_07090.jpg"
 topk = 5
+
+# Set up parameters for entry in command line
+parser = argparse.ArgumentParser()
+parser.add_argument('-c','--checkpoint',
+                    action='store',
+                    type=str,
+                    help='Name of file containing trained model to be loaded and used for predictions.')
+parser.add_argument('-i','--image_path',
+                    action='store',
+                    type=str,
+                    help='Location of image to predict')
+parser.add_argument('-t', '--topk',
+                    action='store',
+                    type=int,
+                    help='Choose quantity of classes visible for predictions.')
+parser.add_argument('-j', '--json',
+                    action='store',
+                    type=str,
+                    help='Name of json file with class names.')
+parser.add_argument('-g','--gpu',
+                    action='store_true',
+                    help='Use GPU if available, if used CUDA in train.py, you must use it again.')
+
+args = parser.parse_args()
+
+# Select parameters entered in command line
+if args.checkpoint:
+    checkpoint = args.checkpoint
+if args.image_path:
+    image_path = args.image_path
+if args.topk:
+    topk = args.topk
+if args.json:
+    filepath = args.json
+if args.gpu:
+    device = 'cuda'
